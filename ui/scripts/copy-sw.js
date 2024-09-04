2ae66b87442b3bc1aefde8b594cf99e8ac11b8ae
Budragchaa <budragchaa@mmusic.mn>
2024-09-04 16:57:11 +0800

    dockerfile

--- ui/scripts/copy-sw.js
+++ ui/scripts/copy-sw.js
@@ -1,8 +1,27 @@
+// import fs from 'fs';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const rootPath = path.join(__dirname, '../../');
+
+// fs.cpSync(path.join(rootPath, 'dist-sw'), path.join(rootPath, 'dist'), { recursive: true });
+
+// fs.rmSync(path.join(rootPath, 'dist-sw'), { recursive: true, force: true });
+
+
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const rootPath = path.join(import.meta.dirname, '../');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootPath = path.join(__dirname, '../../');
+const uiPath = path.join(rootPath, 'ui');
 
-fs.cpSync(path.join(rootPath, 'dist-sw'), path.join(rootPath, 'dist'), { recursive: true });
+// Corrected paths to point to the 'ui' directory
+fs.cpSync(path.join(uiPath, 'dist-sw'), path.join(uiPath, 'dist'), { recursive: true });
 
-fs.rmSync(path.join(rootPath, 'dist-sw'), { recursive: true, force: true });
+fs.rmSync(path.join(uiPath, 'dist-sw'), { recursive: true, force: true });
