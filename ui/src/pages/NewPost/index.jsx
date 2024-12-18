56ad94a1dacb0330e3296b5980a5acf69bc3fcca
atu <oma.atu@gmail.com>
2024-12-18 19:30:49 +0800

    show thumbnail when editing video post

--- ui/src/pages/NewPost/index.jsx
+++ ui/src/pages/NewPost/index.jsx
@@ -603,6 +603,9 @@ const NewPost = () => {
                     onVideoUpload={handleVideoUpload}
                   />
                 )}
+                {isEditPost && video && (
+                  <img src={video.thumbnailURL} style={{ margin: 2, maxWidth: 500, maxHeight: 500, opacity: 0.5 }} />
+                )}
                 {videoThumbnails.length > 0
                   && (<div>
                     {t('new_post.video_thumbnail')}:<br />
@@ -630,7 +633,7 @@ const NewPost = () => {
                       <path d="M0 0h24v24H0V0z" fill="none" />
                       <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-4.86 8.86l-3 3.87L9 13.14 6 17h12l-3.86-5.14z" />
                     </svg>
-                    <p>{t("deleted_image")}</p>
+                    <p>{t("deleted_video")}</p>
                   </div>
                 )}
               </div>
